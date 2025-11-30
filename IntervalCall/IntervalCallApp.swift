f42854b8ee5052a5cbe3354aead22dee95a91c50
Ie So <zousaaaaaan@gmail.com>
2025-11-30 17:32:11 +0900

    Initial Commit: Empty project

---
+++ IntervalCall/IntervalCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalCallApp.swift
+//  IntervalCall
+//
+//  Created by IeSo on 2025/11/30.
+//
+
+import SwiftUI
+
+@main
+struct IntervalCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
